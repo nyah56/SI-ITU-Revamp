63e1385d39740bbd67c73743f5ea67c69bd6e9a8
nyah56 <x6perb@gmail.com>
2025-05-26 15:03:13 +0700

    first commit

---
+++ database/migrations/2025_05_26_070853_consumer.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('consumer', function (Blueprint $table) {
+            $table->id();
+            $table->text('consumer_name');
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->text('address');
+        });
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->text('product_name');
+        });
+        Schema::create('farmer', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->text('address');
+        });
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('consumer_id');
+            $table->foreign('consumer_id')->references('id')->on('consumer')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('product');
+        Schema::dropIfExists('farmer');
+        Schema::dropIfExists('order');
+        Schema::dropIfExists('consumer');
+    }
+};
