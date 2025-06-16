72cb954dbe54f0f06b56b54ceb1415598b32d6ad
nyah56 <x6perb@gmail.com>
2025-06-16 15:53:01 +0700

    add remaining fix */columns.tsx

--- resources/js/components/order/columns.tsx
+++ resources/js/components/order/columns.tsx
@@ -1,22 +1,13 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { ColumnDef } from '@tanstack/react-table';
-import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+import { ArrowUpDown } from 'lucide-react';
+import { ActionColumn } from '../shared/action-column';
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-import ModalDelete from '@/components/shared/modal-delete';
 
 import { Badge } from '@/components/ui/badge';
-import { useState } from 'react';
 export type Order = {
     id: number;
     consumer_name: string;
@@ -76,33 +67,8 @@ export const columns = (onEditClick: (id: number) => void, onDeleteClick: (id: n
     {
         id: 'actions',
         header: 'Actions',
-        cell: ({ row }) => {
-            const order = row.original;
-            const [dropdownOpen, setDropdownOpen] = useState(false);
-
-            return (
-                <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={() => onEditClick(order.id)}>Edit</DropdownMenuItem>
-
-                        <ModalDelete
-                            onDelete={() => {
-                                onDeleteClick(order.id);
-                                setDropdownOpen(false);
-                            }}
-                            trigger={<DropdownMenuItem onSelect={(e) => e.preventDefault()}>Delete</DropdownMenuItem>}
-                        ></ModalDelete>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            );
-        },
+        cell: ({ row }) => (
+            <ActionColumn onEditClick={() => onEditClick(row.original.id)} onDeleteClick={() => onDeleteClick(row.original.id)}></ActionColumn>
+        ),
     },
 ];
